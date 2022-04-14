43f634a57a7d8b1f37d288b481780e4746fa297d
Raxmatillo <milodcomposer@gmail.com>
2022-04-14 14:39:00 +0500

    Test github

--- data/config.py
+++ data/config.py
@@ -1,10 +1,17 @@
-from environs import Env
+# from environs import Env
+#
+# # Теперь используем вместо библиотеки python-dotenv библиотеку environs
+# env = Env()
+# env.read_env()
+#
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
+# ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+# IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+#
+import os
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
-
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+IP = str(os.environ.get("ip"))  # Xosting ip manzili
+# PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
